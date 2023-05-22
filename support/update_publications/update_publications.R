234fa604184d505f41fdf88152d70658ff39bcb8
Dijk <michiel.vandijk@wur.nl>
2023-05-22 10:59:30 +0200

    update publications

---
+++ support/update_publications/update_publications.R
@@ -0,0 +1,50 @@
+# ========================================================================================
+# Project:  michielvandijk.org
+# Subject:  updates publications
+# Author:   Michiel van Dijk
+# Contact:  michiel.vandijk@wur.nl
+# ========================================================================================
+
+# ========================================================================================
+# SETUP ----------------------------------------------------------------------------------
+# ========================================================================================
+
+# Load pacman for p_load
+if(!require(pacman)) install.packages("pacman")
+library(pacman)
+
+# Load key packages
+p_load(here)
+
+# R options
+options(scipen = 999)
+options(digits = 4)
+
+# ========================================================================================
+# TO ADD ---------------------------------------------------------------------------------
+# ========================================================================================
+
+# Fix special symbols in Mendeley/LATEX
+# write function to add figure
+
+
+# ========================================================================================
+# UPDATE PUBLICATIONS---------------------------------------------------------------------
+# ========================================================================================
+# Source function
+source(here::here("support/update_publications/bibtex_2_hugo.R"))
+
+# Set input and output file
+bibfile <- "C:/data/mendeley/MAGNET_pubs.bib"
+outfold   <- here::here("content/publication/")
+
+# process
+bibtex2academic(bibfile  = bibfile, outfold   = outfold)
+
+
+# ========================================================================================
+# UPDATE CV ------------------------------------------------------------------------------
+# ========================================================================================
+
+# cv_source <- "C:/data/github/CV/cv/michiel_van_dijk_cv_long.pdf"
+# file.copy(cv_source, here::here("static/cv/michiel_van_dijk_cv_long.pdf"), overwrite = TRUE)
